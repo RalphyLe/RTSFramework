59d02c245b8ec92b5a560fea7f012b805267d898
RalphyLe <42488575+RalphyLe@users.noreply.github.com>
2020-06-12 18:42:30 +0800

    添加Task模块

---
+++ Assets/Battle Simulator/Assets/Framework/Scripts/Runtime/Base/Task/ITask.cs
@@ -0,0 +1,21 @@
+﻿namespace Framework.Runtime
+{
+    /// <summary>
+    /// 任务接口
+    /// </summary>
+    public interface ITask
+    {
+
+        /// <summary>
+        /// 任务序列ID
+        /// </summary>
+        int SerialId { get; }
+
+        /// <summary>
+        /// 任务是否完成
+        /// </summary>
+        bool Done { get; }
+
+    }
+
+}
