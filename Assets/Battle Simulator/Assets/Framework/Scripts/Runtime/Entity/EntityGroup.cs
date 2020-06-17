7bf0c4cd4496b388a4559f0353044ad2ca724cad
RalphyLe <42488575+RalphyLe@users.noreply.github.com>
2020-06-17 14:16:20 +0800

    添加Entity模块

---
+++ Assets/Battle Simulator/Assets/Framework/Scripts/Runtime/Entity/EntityGroup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Framework.Runtime
+{
+    /// <summary>
+    /// 实体组
+    /// </summary>
+    public class EntityGroup : IEntityGroup
+    {
+        
+    } 
+}
---
+++ Assets/Battle Simulator/Assets/Framework/Scripts/Runtime/Entity/IEntityGroupHelper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Framework.Runtime
+{
+    public interface IEntityGroupHelper
+    {
+        /// <summary>
+        /// 实体组辅助器接口。
+        /// </summary>
+        public interface IEntityGroupHelper
+        {
+        }
+    } 
+}
